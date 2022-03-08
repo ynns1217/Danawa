e3ecd107bba9d2e68d34e09e2bdf37040e7aeb81
ynns1217 <ynns1217@naver.com>
2022-03-08 16:56:00 +0900

    함수사용

--- 현재고현황main.c
+++ 현재고현황main.c
@@ -81,6 +81,14 @@ void insert_table()
 }
 void edit_table()
 {
+	if (initalizing("C:\\Users\\PKNU\\source\\repos\\Danawa\\Stock_In") == -1)
+	{
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
 	char* conditional = "num_item=800";
 	char* set = "chulgo_item=5";
 
@@ -93,6 +101,15 @@ void edit_table()
 	}
 }
 int jaego_count() {
+	printf("\t총 재고 수량\n");
+	printf("======================================\n");
+
+	if (initalizing("C:\\Users\\PKNU\\source\\repos\\Danawa\\Stock_In") == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
 	char temp_insert[100];
 	char Select_IT[100];
 
@@ -201,6 +218,137 @@ int jaego_count() {
 	system("pause");
 	printf("\n\n");
 	file_column_free();
+	result_free(_result, result_count);
+
+} 		//총 재고 수량
+void ibgo_chulgo_count()		
+{
+	printf("\t재고 현황\n");
+	printf("======================================\n");
+	print_data();
+	printf("\n");
+	file_column_free();
+	system("pause");
+}	   //재고관리에 재고현황
+void jaego_print()
+{
+	if (initalizing("C:\\Users\\PKNU\\source\\repos\\Danawa\\Stock_In") == -1) {
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+	char temp_insert[100];
+	char Select_IT[100];
+
+	printf("검색할 품번을 입력하시오\n");
+	scanf("%s", &Select_IT);
+
+	char select_num_item[100] = "num_item=";
+	char Parameter_Insert[1000] = { NULL };
+	char temp_int[20] = { 0 };
+	strcat(select_num_item, Select_IT);	// select함수에 조건으로 입력하기 위한 양식만들기
+												// ex) 여기까지 [select_num_warehouse = num_warehouse=1999]
+
+	if (_select(select_num_item, "name_item, num_item, date, gicho_item, ibgo_item, chulgo_item, jaego_item", &select_result_str) == -1) {	// 만든 양식을 토대로 창고목록에서 내부 칼럼정보를 선택해 받아옴
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	else
+	{
+		printf("======================================\n");
+	}
+
+	//_select함수에서 저장된 문자열(select_result_str)을 result_column 구조체에 해당 타입에 맞춰 배열 형태로 정리하여 저장
+
+	if ((result_count = recv_result(&_result, select_result_str)) == -1) {		// select로 받아온 정보를 _result 포인트 구조체에 멤버로 저장함
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	strcat(Parameter_Insert, "\'");							// 문자열의 경우 앞뒤로 작은따옴표 필요
+	strcat(Parameter_Insert, *(_result->_string_data));		// 받아온 결과를 저장
+	strcat(Parameter_Insert, "\' ");						// 문자열의 경우 앞뒤로 작은따옴표 필요 + 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' ]
+
+	itoa(*(_result->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+
+	itoa(*(_result->next->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+	itoa(*(_result->next->next->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+	itoa(*(_result->next->next->next->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+	itoa(*(_result->next->next->next->next->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+
+
+	//총 재고 수량 계산해서 출력
+	*(_result->next->next->next->next->next->next->_int_data) = *(_result->next->next->next->_int_data) + *(_result->next->next->next->next->_int_data) - *(_result->next->next->next->next->next->_int_data);
+
+	itoa(*(_result->next->next->next->next->next->next->_int_data), temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, " ");							// 끝났음을 의미하는 띄어쓰기
+
+															// ex) 여기까지 [Parameter_Insert = 'warehouse1' 1999 ]
+
+
+
+	if ((find = find_result(_result, "jaego_item")) == -1)
+	{
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		result_free(_result, result_count);
+		return -1;
+
+	}
+
+	printf("     %s\n", find->name);
+	printf("=============\n");
+
+	for (int i = 0; i < result_count; i++)
+	{
+		if (int_is_null(find->_int_data[i]))
+			printf("     (null)\n");
+		else
+		{
+			printf("     %d\n", find->_int_data[i]);
+		}
+		break;
+
+		return find->_int_data[i];
+	}
+
+	printf("======================================\n");
+	printf("%s\n", Parameter_Insert);
+	system("pause");
+	printf("\n\n");
+	file_column_free();
+}
+void jaego_use()
+{
 
 }
 
@@ -213,18 +361,12 @@ int main() {
 		table_init();
 		insert_table();
 
-		/// ///////////////////////////////////////////////////////////////////
-		/// ///////////////////////////////////////////////////////////////////
-
 		printf("\t재고관리\n");
 		printf("1.현 재고 현황 2.재고 이동 \n");
 		printf("======================================\n");
 		scanf("%d", &option1);
 
 		system("cls");
-
-		/// ///////////////////////////////////////////////////////////////////
-		/// ///////////////////////////////////////////////////////////////////
 
 		switch (option1)
 		{
@@ -241,30 +383,14 @@ int main() {
 			/// 
 			if (option1 == 1)
 			{
-				printf("\t재고 현황\n");
-				printf("======================================\n");
-				print_data();
-				printf("\n");
-				file_column_free();
-				system("pause");
+				//재고관리에 재고현황
+				ibgo_chulgo_count();
 				break;
 			}
 
 			else if (option1 == 2)
 			{
-				printf("\t총 재고 수량\n");
-				printf("======================================\n");
-
-				if (initalizing("C:\\Users\\PKNU\\source\\repos\\Danawa\\Stock_In") == -1) {
-					printf("%s\n", err_msg);
-
-					file_column_free();
-					return -1;
-				}
-
 				jaego_count();
-				system("pause");
-				result_free(_result, result_count);
 
 				break;
 			}
@@ -273,7 +399,7 @@ int main() {
 			printf("\t재고 사용하기 \n");
 			printf("======================================\n");
 
-			jaego_count();
+			jaego_print();
 
 			for (int i = 0; i < result_count; i++)
 			{
@@ -282,13 +408,6 @@ int main() {
 					printf("======================================\n");
 					printf("생산하러 가자이\n");
 
-					if (initalizing("C:\\Users\\PKNU\\source\\repos\\Danawa\\Stock_In") == -1)
-					{
-						printf("%s\n", err_msg);
-
-						file_column_free();
-						return -1;
-					}
 					edit_table();
 					jaego_count();
 				}
