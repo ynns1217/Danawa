8c73c490d87bbac91e457429ed5d5cf341f5ce37
ynns1217 <ynns1217@naver.com>
2022-03-10 04:39:50 +0900

    입고 받아 올때 마다 날짜 업데이트

--- jaego_fuc.c
+++ jaego_fuc.c
@@ -7,15 +7,9 @@
 
 #pragma warning(disable:4996)
 
-
-
-
 result* _result;
 result* find;
 int result_count;
-
-int num = 3;
-
 
 
 //최종 재고 수량 출력해서 조회
@@ -31,12 +25,15 @@ int jaego_print()
 	timer = time(NULL);
 	t = localtime(&timer);
 
+	int today_date = 0;
+	today_date = ((t->tm_year + 1900) * 10000) + ((t->tm_mon + 1) * 100) + (t->tm_mday);	// 오늘 날짜( 20220304 꼴 )
+
 
 	char Parameter_Insert[1000] = { NULL };
 	char temp_int[20] = { 0 };
 	char Select_BuyingList_num[20] = { 0 };
 
-	// 발주리스트 출력 및 정보 받아오기
+	
 	if (initalizing("Jaego") == -1)
 	{
 		printf("%s\n", err_msg);
@@ -87,9 +84,9 @@ int jaego_print()
 
 
 		//date
-		itoa(*(_result->next->next->next->_int_data), temp_int, 10);
-		strcat(Parameter_Insert, temp_int);
-		strcat(Parameter_Insert, ", ");
+		itoa(today_date, temp_int, 10);							// 받아온 정보가 int형이므로 문자열로 형변환수행
+		strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+		strcat(Parameter_Insert, ", ");							// 끝났음을 의미하는 띄어쓰기
 
 
 
@@ -148,8 +145,6 @@ int jaego_print()
 	file_column_free();
 }
 
-
-
 //입고 정보를 불러와서 입고재고에 업데이트
 void ibgo_jaego_print1()
 {
@@ -164,14 +159,16 @@ result* _result;
 	timer = time(NULL);
 	t = localtime(&timer);
 
-	
+	int today_date = 0;
+	today_date = ((t->tm_year + 1900) * 10000) + ((t->tm_mon + 1) * 100) + (t->tm_mday);	// 오늘 날짜( 20220304 꼴 )
+
 	char Parameter_Insert[1000] = { NULL };				
 	char temp_int[20] = { 0 };							
 	char Select_BuyingList_num[20] = { 0 };				
 	
 
 	
-	// 발주리스트 출력 및 정보 받아오기
+	// 입고리스트 출력 및 정보 받아오기
 	if (initalizing("In_WareHouse") == -1)	
 	{
 		printf("%s\n", err_msg);
@@ -226,9 +223,10 @@ result* _result;
 
 
 	//date
-	itoa(*(_result->next->next->next->_int_data), temp_int, 10);		
-	strcat(Parameter_Insert, temp_int);						
-	strcat(Parameter_Insert, ", ");							
+	itoa(today_date, temp_int, 10);							// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(Parameter_Insert, temp_int);						// 변환한 문자열을 Parameter_Insert에 붙임
+	strcat(Parameter_Insert, ", ");							// 끝났음을 의미하는 띄어쓰기
+
 
 
 
@@ -252,14 +250,29 @@ result* _result;
 		return -1;
 	}
 
-	char update_rest_num[100] = "ibgo_item=";
+	char update_num[100] = "ibgo_item=";
 
 	itoa(num_InWarehouse, temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
-	strcat(update_rest_num, temp_int);				
+	strcat(update_num, temp_int);
 
 
-	// 입고한 다음 입고 수량 을 업데이트한다
-	if (_update(select_num_BuyList, update_rest_num) == -1)
+	// 입고한 다음 입고 재고 수량 을 업데이트한다
+	if (_update(select_num_BuyList, update_num) == -1)
+	{
+		printf("%s\n", err_msg);
+
+		file_column_free();
+		return -1;
+	}
+
+	char update_time[100] = "date=";
+
+	itoa(today_date, temp_int, 10);		// 받아온 정보가 int형이므로 문자열로 형변환수행
+	strcat(update_time, temp_int);
+
+
+	// 입고한 다음 입고 재고 수량 을 업데이트한다
+	if (_update(select_num_BuyList, update_time) == -1)
 	{
 		printf("%s\n", err_msg);
 
@@ -270,12 +283,10 @@ result* _result;
 	jaego_print();
 }
 
-
 //출고 정보를 불러와서 출고재고에 업데이트
-void ibgo_chulgo_print()
+void chulgo_jaego_print()
 {
 
-	
 }
 
 
